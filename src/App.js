66b65e87d8a03ff28df0c7df8b4e72d7e25e87bd
Suraj Singh <mesuraj30@gmail.com>
2020-02-07 20:36:20 +0530

    deleted react logos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import { Switch,Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return (
+  <div>
+    <h1>HatsPage</h1>
+    </div>
+  )
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/' component={HomePage} />
     </div>
   );
 }
