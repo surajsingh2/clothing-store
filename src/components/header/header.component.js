a9a005c31122236f275365ef9d0dc5897ea1fa49
Suraj Singh <mesuraj30@gmail.com>
2020-02-08 20:25:28 +0530

    redux 1

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { ReactComponent as Logo} from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import './header.styles.scss'
@@ -18,14 +19,20 @@ const Header = ({currentUser}) => {
                 CONTACT
             </Link>
             {
-                currentUser ?
+                currentUser ? (
                 <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
-                :
+                ):(
                 <Link className='option' to='/signin'>SIGN IN</Link>
+                )
             }
         </div>
     </div>
   )
 }
 
-export default Header
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+
+})
+
+export default connect(mapStateToProps)(Header); 
--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -39,11 +39,16 @@ class SignIn extends Component {
           <h2>I already have an account</h2>
           <span>Sign in with your email and password</span>
           <form onSubmit={this.handleSubmit}>
-              <FormInput name="email" type="email" label="email" handleChange={this.handleChange} value={this.state.email} required />
-              <FormInput name="password" type="password" label="password" handleChange={this.handleChange} value={this.state.password} required />
+              <FormInput name="email" type="email" label="email"
+               handleChange={this.handleChange} 
+               value={this.state.email} required />
+              <FormInput name="password" type="password" label="password"
+               handleChange={this.handleChange} 
+               value={this.state.password} required />
               <div className='buttons'>
               <CustomButton type="submit" >Sign In </CustomButton>
-              <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign In With Google </CustomButton>
+              <CustomButton onClick={signInWithGoogle} 
+              isGoogleSignIn >Sign In With Google </CustomButton>
               </div>
           </form>
       </div>
