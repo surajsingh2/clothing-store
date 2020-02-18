46d9680f236df47e95d2ffa2a4091ba1513cdee0
Suraj Singh <mesuraj30@gmail.com>
2020-02-18 19:38:13 +0530

    cart item comoponent added

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemsToAdd) => {
+    const existingCartItems = cartItems.find(
+        cartItem => cartItem.id === cartItemsToAdd.id
+    );
+
+    if (existingCartItems) {
+        return cartItems.map(cartItem => 
+            cartItem.id === cartItemsToAdd.id ? {
+                ...cartItem, quantity: cartItem.quantity + 1 
+            } : cartItem
+            )
+    }
+
+    else {
+        return [...cartItems, { ...cartItemsToAdd, quantity: 1}]
+    }
+}
